7a8a24106a390d0074ac8d2b827426fd942d43c7
tmthu <taminhthunkh@gmail.com>
2023-11-14 11:49:47 +0700

    thu

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ HOST = data["MailServer"]
 SEND_PORT = int(data["SMTP"])
 RECV_PORT = int(data["POP3"])
 AUTOLOAD = data["Autoload"]
-
+#confict
 #MAIL CLIENT CONSOLE
 
 while True:
@@ -25,7 +25,5 @@ while True:
     elif (choice == "2"):
         #READ EMAIL
         readEmail(EMAIL, PASSWORD, HOST, RECV_PORT)
-
     elif (choice == "3"):
         exit(0)
-#hehe
