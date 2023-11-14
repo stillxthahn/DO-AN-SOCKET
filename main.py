53bdde2a6e80c86174546f8890179788e439633c
stillxthahn <lxthanh235@gmail.com>
2023-11-14 10:42:39 +0700

    PUSH FINAL

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
-#alo alo alo staging area
+from readConfig import readConfig
+from sendEmail import sendEmail
+from readEmail import readEmail
+#READ CONFIG FILE
+data = readConfig()
+USERNAME = data["Username"]
+EMAIL = data["Email"]
+PASSWORD = data["Password"]
+HOST = data["MailServer"]
+SEND_PORT = int(data["SMTP"])
+RECV_PORT = int(data["POP3"])
+AUTOLOAD = data["Autoload"]
 
-#test 2
+#MAIL CLIENT CONSOLE
 
-#haha
+while True:
+    print("Vui lòng chọn Menu:\r\n")
+    print("1. Để gửi email\r\n")
+    print("2. Để xem danh sách các email đã nhận\r\n")
+    print("3. Thoát\r\n")
+    choice = input("Bạn chọn: ")
+    if (choice == "1"):
+        #SEND EMAIL
+        sendEmail(USERNAME, EMAIL, HOST, SEND_PORT)
+    elif (choice == "2"):
+        #READ EMAIL
+        readEmail(EMAIL, PASSWORD, HOST, RECV_PORT)
+    elif (choice == "3"):
+        exit(0)
+
+
+
