8550d792f20f803ba9661048aefef4ac90e14962
giathiec <giathiecluong123@gmail.com>
2023-11-17 22:40:09 +0700

    update read_file_json 17/11

--- read_json_file.py
+++ read_json_file.py
@@ -1,6 +1,8 @@
 import json
 def read_json_file(filename):
     with open(filename, 'r') as f:
-        listdata = json.loads(f.read())
-    return listdata
-#thiecs
+        try:
+            listdata = json.loads(f.read())
+            return listdata
+        except Exception as JSONDecodeError:
+            return []
