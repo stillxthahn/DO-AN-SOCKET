8f6d967b2a34d682a776663b40f2293e835e9cc0
giathiec <giathiecluong123@gmail.com>
2023-11-25 00:44:39 +0700

    thiec list folder + read email in folder

--- Recv_list.py
+++ Recv_list.py
@@ -1,24 +1,33 @@
 from read_json_file import read_json_file
 import json
-
-def return_json():
+import os
+def output_receive_list(foldername):
     listjson = read_json_file('Email_Infor.json')
-    return listjson
-def output_receive_list(listjson):
-    listjson = read_json_file()
-    for i in range(len(listjson)):
+    folder_path = os.path.join(os.getcwd(),"local_mailbox", foldername)
+    files_arr = os.listdir(folder_path)
+    files_arr.reverse()
+    for i in range(len(files_arr)):
         print(i+1, end=' ')
-        if listjson[i]["status"] != True:
-            print("(chua doc)", end = ' ')
-            print (listjson[i]["from"], end =", ")
-            print (listjson[i]["subject"])
-        else:
-            print (listjson[i]["from"], end =", ")
-            print (listjson[i]["subject"])
+        for j in range(len(listjson)):
+            if listjson[j]["Filename"] == files_arr[i]:
+                if listjson[j]["Status"] == '0':
+                    print("(chua doc)", end = ' ')
+                print (listjson[j]["From"], end =", ")
+                print (listjson[j]["Subject"])
+def read_chosen_file(foldername, choice):
+    folder_path = os.path.join(os.getcwd(),"local_mailbox", foldername)
+    files_arr = os.listdir(folder_path)
+    files_arr.reverse()
+    file_path = os.path.join(folder_path, files_arr[int (choice) -1])
+    with open(file_path) as msgfile:
+        readfile = msgfile.read()
+        print ("Nội dung của email thứ {choice}: " , readfile)
+
+
 def update_status(listjson, choice):
     if 1 <= int(choice) <= len(listjson):
         selected_email = listjson[int(choice) - 1]
-        selected_email["status"] = 1
+        selected_email["Status"] = 1
         return True
     else:
         return False
